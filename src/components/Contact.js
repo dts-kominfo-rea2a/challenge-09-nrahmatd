231314996feda985560f2e963edff69daa9421a8
nur.rahmat <nur.rahmat@aksaramay.com>
2022-06-26 22:19:50 +0700

    Done Task Intro React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,16 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className='card'>
+            <img src={props.data.photo} alt={props.data.name}/>
+            <div className='container'>
+                <h1>{props.data.name}</h1>
+                <h4>{props.data.phone}</h4>
+                <h4>{props.data.email}</h4>
+            </div>
+        </div>
     )
 }
 
